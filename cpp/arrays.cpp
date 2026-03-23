f17c8c706d82eb6daa06286a09fcd5deaefa2d4c
02Anmol <tyagianmol839@gmail.com>
2026-03-23 23:47:33 +0530

    array in cpp

---
+++ cpp/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[5] = {12,34,45,67,89};
+    cout<< marks[0]<<endl;
+
+    for(int i=0;i<5;i++){
+        cout<<marks[i]<<endl;
+    }
+    return 0;
+}
